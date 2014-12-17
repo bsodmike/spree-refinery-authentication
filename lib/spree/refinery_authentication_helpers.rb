06f4b2a6bf3739a59209c1524667b278628a8206
Michael de Silva <michael@mwdesilva.com>
2014-12-17 15:11:35 +0530

    Rename `AuthenticationHelpers` to prevent clashing with `spree_auth_devise`

---
+++ lib/spree/refinery_authentication_helpers.rb
@@ -0,0 +1,32 @@
+module Spree
+  module RefineryAuthenticationHelpers
+    def self.included(_klass)
+      _klass.class_eval do
+        include InstanceMethods
+        helper_method :spree_login_path,
+          :spree_signup_path,
+          :spree_logout_path,
+          :spree_current_user,
+          :refinery_user?
+      end
+    end
+
+    module InstanceMethods
+      def spree_current_user
+        current_refinery_user
+      end
+
+      def spree_login_path
+        refinery.new_refinery_user_session_path
+      end
+
+      def spree_signup_path
+        refinery.new_refinery_user_registration_path
+      end
+
+      def spree_logout_path
+        refinery.destroy_refinery_user_session_path
+      end
+    end
+  end
+end
--- lib/spree_refinery_authentication/engine.rb
+++ lib/spree_refinery_authentication/engine.rb
@@ -1,4 +1,7 @@
-require "spree/authentication_helpers"
+puts "Hooking Spree into Refinery => Spree::BaseController - #{__FILE__[/([app\/\w\.]+|vendor[\/\w\.]+)/]}"
+puts "Hooking Spree into Refinery => Spree::Api::BaseController - #{__FILE__[/([app\/\w\.]+|vendor[\/\w\.]+)/]}"
+puts "Hooking Spree into Refinery => ApplicationController - #{__FILE__[/([app\/\w\.]+|vendor[\/\w\.]+)/]}"
+require "spree/refinery_authentication_helpers"
 
 module SpreeRefineryAuthentication
   class Engine < Rails::Engine
@@ -10,7 +13,9 @@ module SpreeRefineryAuthentication
       WillPaginate::ActiveRecord::RelationMethods.send :alias_method, :per, :per_page
       WillPaginate::ActiveRecord::RelationMethods.send :alias_method, :num_pages, :total_pages
 
-      ApplicationController.send :include, Spree::AuthenticationHelpers
-    end  
+      Spree::BaseController.send(:include, Spree::RefineryAuthenticationHelpers)
+      Spree::Api::BaseController.send(:include, Spree::RefineryAuthenticationHelpers)
+      ApplicationController.send(:include, Spree::RefineryAuthenticationHelpers)
+    end
   end
 end
