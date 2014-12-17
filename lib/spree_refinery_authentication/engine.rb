34b0dd0a536f4dd0b841b068b97823315c40072b
Michael de Silva <michael@mwdesilva.com>
2014-12-17 15:47:17 +0530

    Alias Kaminari call in Spree with Refinery's WillPaginate equivalent

--- lib/spree_refinery_authentication/engine.rb
+++ lib/spree_refinery_authentication/engine.rb
@@ -12,6 +12,7 @@ module SpreeRefineryAuthentication
 
       WillPaginate::ActiveRecord::RelationMethods.send :alias_method, :per, :per_page
       WillPaginate::ActiveRecord::RelationMethods.send :alias_method, :num_pages, :total_pages
+      WillPaginate::ActiveRecord::RelationMethods.send :alias_method, :prev_page, :previous_page
 
       Spree::BaseController.send(:include, Spree::RefineryAuthenticationHelpers)
       Spree::Api::BaseController.send(:include, Spree::RefineryAuthenticationHelpers)
